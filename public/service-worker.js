dd7ae23c1006e81e2a58f871c2b77f986a7ce19b
ScriptSculptor007 <scriptsculptor007@gmail.com>
2024-01-08 21:26:03 +0530

    SErvice worker update for push notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("push", (event) => {
   const data = event.data.json(); // Assuming the push notification data is sent as JSON
@@ -17,5 +18,29 @@ self.addEventListener("push", (event) => {
       data.body || "Default Notification Title",
       options
     )
+  );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("notificationclick", function (event) {
+  let url = "https://youtube.com";
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    // eslint-disable-next-line no-restricted-globals, no-undef
+    clients.matchAll({ type: "window" }).then((windowClients) => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        // If so, just focus it.
+        if (client.url === url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      // eslint-disable-next-line no-undef
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
   );
 });
